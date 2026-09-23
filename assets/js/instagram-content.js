4e41b0d279f2036fb8d817257fd4e3c4f8dd4c27
kadmontech-hub <kadmontech@gmail.com>
2026-09-22 23:52:49 -0300

    Add curated Instagram content model

---
+++ assets/js/instagram-content.js
@@ -0,0 +1,46 @@
+window.MUNDO_BIOHACK_INSTAGRAM = Object.freeze({
+  profileUrl: "https://www.instagram.com/mundobiohack/",
+  handle: "@mundobiohack",
+  items: [
+    {
+      id: "instagram-reels",
+      type: "Reels",
+      title: "Reels y explicaciones breves",
+      description: "Ideas, conceptos y piezas cortas para seguir Mundo Biohack entre publicaciones largas.",
+      thumbnail: "https://i.imgur.com/1HPC9Gk.png",
+      focalDesktop: "50% 38%",
+      focalMobile: "50% 34%",
+      url: "https://www.instagram.com/mundobiohack/"
+    },
+    {
+      id: "instagram-conversaciones",
+      type: "Conversaciones",
+      title: "Entrevistas y conversaciones",
+      description: "Fragmentos, intercambios y personas que amplían la conversación sobre biohacking.",
+      thumbnail: "https://i.imgur.com/73PRkHI.png",
+      focalDesktop: "50% 42%",
+      focalMobile: "50% 38%",
+      url: "https://www.instagram.com/mundobiohack/"
+    },
+    {
+      id: "instagram-tecnologia",
+      type: "Tecnología",
+      title: "Tecnología y experiencias",
+      description: "Herramientas, pruebas visuales y formas de explorar tecnología aplicada al bienestar.",
+      thumbnail: "https://i.imgur.com/OqAuFzB.png",
+      focalDesktop: "50% 48%",
+      focalMobile: "50% 44%",
+      url: "https://www.instagram.com/mundobiohack/"
+    },
+    {
+      id: "instagram-actualidad",
+      type: "Actualidad",
+      title: "Novedades y piezas del día a día",
+      description: "La capa más inmediata del proyecto: publicaciones, historias y Reels desde la cuenta oficial.",
+      thumbnail: "https://i.imgur.com/CPtr2Io.png",
+      focalDesktop: "50% 40%",
+      focalMobile: "50% 36%",
+      url: "https://www.instagram.com/mundobiohack/"
+    }
+  ]
+});
